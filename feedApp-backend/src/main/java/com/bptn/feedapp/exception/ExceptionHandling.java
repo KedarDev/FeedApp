6d9db1dcb624fb87d8d6036133b40ca970626567
KedarDev <123585878+KedarDev@users.noreply.github.com>
2023-06-28 22:29:56 -0400

    Update ExceptionHandling.java

--- feedApp-backend/src/main/java/com/bptn/feedapp/exception/ExceptionHandling.java
+++ feedApp-backend/src/main/java/com/bptn/feedapp/exception/ExceptionHandling.java
@@ -58,7 +58,7 @@ public class ExceptionHandling implements ErrorController {
 	private static final String NO_MAPPING_EXIST_URL = "There is no mapping for this URL";
 	private static final String ERROR_PATH = "/error";
 	
-	// HTTP Resoinse Method
+	// HTTP Response Method
 	private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
 	    return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus, // HTTP instence 
 	    			httpStatus.getReasonPhrase().toUpperCase(), message), httpStatus);
